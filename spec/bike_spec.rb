7a750edc650814eda70822efdaf8c9c6bfdb568e
Sean Stevens <seanstevens@Seans-MacBook-Pro.local>
2014-09-15 18:19:06 +0100

    Update to rspec tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,4 +7,10 @@ describe Bike do
 		the_bike = Bike.new
 		expect(the_bike).not_to be_broken
 	end
+
+	it "should be able to be broken" do
+		the_bike = Bike.new
+		the_bike.break!
+		expect(the_bike).to be_broken
+	end
 end
