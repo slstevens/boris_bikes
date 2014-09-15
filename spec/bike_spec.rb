d8adb4c421b95b9eb24aacbbb87006a7068de823
Sean Stevens <seanstevens@Seans-MacBook-Pro.local>
2014-09-15 18:43:04 +0100

    After refactoring

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,19 +3,19 @@ require 'bike'
 
 # we're describing the functionality of a specific class, Bike
 describe Bike do
+
+	let(:the_bike) {Bike.new}
+
 	it "should not be broken after we create it" do
-		the_bike = Bike.new
 		expect(the_bike).not_to be_broken
 	end
 
 	it "should be able to be broken" do
-		the_bike = Bike.new
 		the_bike.break!
 		expect(the_bike).to be_broken
 	end
 
 	it "should be fixable" do
-		the_bike = Bike.new
 		the_bike.break!
 		the_bike.fix!
 		expect(the_bike).not_to be_broken
